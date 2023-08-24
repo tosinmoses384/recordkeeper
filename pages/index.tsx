1c08e142a0be2e236a84f5832ddd0a6f36e416a5
Israel <tosinmoses384@gmail.com>
2023-08-24 15:09:09 +0100

    feat(tailwind): install and setup tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,34 +12,47 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
+        <h1 
+        // className={styles.title}
+        className='text-[red]'
+        >
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p 
+        // className={styles.description}
+        >
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div 
+        // className={styles.grid}
+        >
+          <a href="https://nextjs.org/docs" 
+          // className={styles.card}
+          >
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" 
+          // className={styles.card}
+          >
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            // className={styles.card}
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a href="https://vercel.com/new" className={styles.card}>
+          <a href="https://vercel.com/new" 
+          // className={styles.card}
+          >
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -55,7 +68,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span 
+          // className={styles.logo}
+          >
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
