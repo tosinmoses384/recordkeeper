889afb6ce5906de554db551b103533ddf15216b4
Israel <tosinmoses384@gmail.com>
2023-08-25 17:57:48 +0100

    fix(refactor): clean up and refactor code base

--- components/common/Main.tsx
+++ components/common/Main.tsx
@@ -6,112 +6,98 @@ const Main = () => {
   const [individual, setIndividual] = useState([
     {
       _id: "1",
-      NationalID: "1692869557",
+      NationalID: "1692869743557",
       title: "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      teacherNumber: "mellow pop, enhanced with  iconic costumes",
-      salary: "mellow pop, enhanced with  iconic costumes",
+      firstname: "Abigael",
+      surname: "Lebanon",
+      date: "1987-08-17T09:30:09.988Z",
+      teacherNumber: "3727372838",
+      salary: "N280000",
     },
     {
       _id: "2",
-      NationalID: "1692869557",
+      NationalID: "16928182869557",
       title: "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      teacherNumber: "mellow pop, enhanced with  iconic costumes",
-      salary: "mellow pop, enhanced with  iconic costumes",
+      firstname: "Nurudeen",
+      surname: "Afolabi",
+      date: "1987-08-17T09:30:09.988Z",
+      teacherNumber: "3737181381",
+      salary: "N300000",
     },
     {
       _id: "3",
-      NationalID: "1692869557",
+      NationalID: "16928699293557",
       title: "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      teacherNumber: "mellow pop, enhanced with  iconic costumes",
-      salary: "mellow pop, enhanced with  iconic costumes",
+      firstname: "Gaga",
+      surname: "Isaac",
+      date: "1988-08-17T09:30:09.988Z",
+      teacherNumber: "278281813",
+      salary: "N350000",
     },
     {
       _id: "4",
-      NationalID: "1692869557",
+      NationalID: "1692819369557",
       title: "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      teacherNumber: "mellow pop, enhanced with  iconic costumes",
-      salary: "mellow pop, enhanced with  iconic costumes",
+      firstname: "Stephen",
+      surname: "Oriade",
+      date: "1989-08-17T09:30:09.988Z",
+      teacherNumber: "91817373",
+      salary: "N300000",
     },
     {
       _id: "5",
-      NationalID: "1692869557",
+      NationalID: "1692860839557",
       title: "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      teacherNumber: "mellow pop, enhanced with  iconic costumes",
-      salary: "mellow pop, enhanced with  iconic costumes",
+      firstname: "Thompson",
+      surname: "Ogoni",
+      date: "1985-08-17T09:30:09.988Z",
+      teacherNumber: "38282823737",
+      salary: "N150000",
     },
   ]);
+
   const [fetchedStudent, setFetchedStudent] = useState([
     {
-      _id: "1",
-      NationalID: "1692869557",
-      // "title": "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      studentNumber: "mellow pop, enhanced with  iconic costumes",
-      // "salary": "mellow pop, enhanced with  iconic costumes",
+      _id: "999",
+      NationalID: "169286938291557",
+      firstname: "John",
+      surname: "Ude",
+      date: "2005-08-17T09:30:09.988Z",
+      studentNumber: "98738772",
     },
     {
-      _id: "2",
-      NationalID: "1692869557",
-      // "title": "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      studentNumber: "mellow pop, enhanced with  iconic costumes",
-      // "salary": "mellow pop, enhanced with  iconic costumes",
+      _id: "998",
+      NationalID: "16928610399557",
+      firstname: "Claron",
+      surname: "Mary",
+      date: "2004-08-17T09:30:09.988Z",
+      studentNumber: "3736288222",
     },
     {
-      _id: "3",
-      NationalID: "1692869557",
-      // "title": "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      studentNumber: "mellow pop, enhanced with  iconic costumes",
-      // "salary": "mellow pop, enhanced with  iconic costumes",
+      _id: "997",
+      NationalID: "16928698374557",
+      firstname: "Magaret",
+      surname: "Olufande",
+      date: "2006-08-17T09:30:09.988Z",
+      studentNumber: "3732811919",
     },
     {
-      _id: "4",
-      NationalID: "1692869557",
-      // "title": "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      studentNumber: "mellow pop, enhanced with  iconic costumes",
-      // "salary": "mellow pop, enhanced with  iconic costumes",
+      _id: "996",
+      NationalID: "16928695591847",
+      firstname: "Rebon",
+      surname: "Uzziah",
+      date: "2007-08-17T09:30:09.988Z",
+      studentNumber: "19192998383",
     },
     {
-      _id: "5",
-      NationalID: "1692869557",
-      // "title": "Mr.",
-      firstname: "Six Bears Running",
-      surname: "mellow pop, enhanced with  iconic costumes",
-      date: "2023-08-17T09:30:09.988Z",
-      studentNumber: "mellow pop, enhanced with  iconic costumes",
-      // "salary": "mellow pop, enhanced with  iconic costumes",
+      _id: "995",
+      NationalID: "1692869173843557",
+      firstname: "Alibaba",
+      surname: "Bamaba",
+      date: "2009-08-17T09:30:09.988Z",
+      studentNumber: "8838727474747377",
     },
   ]);
-
-
-
-
-
 
   const [iTab, setTab] = useState("Teacher");
   const [showModal, setshowModal] = useState(false);
@@ -157,9 +143,9 @@ const Main = () => {
           setshowModal={setshowModal}
           iTab={iTab}
           individual={individual}
-            setIndividual={setIndividual}
-            fetchedStudent={fetchedStudent}
-            setFetchedStudent={setFetchedStudent}
+          setIndividual={setIndividual}
+          fetchedStudent={fetchedStudent}
+          setFetchedStudent={setFetchedStudent}
         />
       )}
     </>
