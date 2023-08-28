b3abdbaf2fbc8879f4d65ca70e92ec2c3fc1c246
Israel <tosinmoses384@gmail.com>
2023-08-28 03:29:52 +0100

    feat(empty-state): add empty state to app

---
+++ components/common/EmptyRaiseExplore.tsx
@@ -0,0 +1,13 @@
+import Router from 'next/router';
+
+
+export const EmptyRaiseExplore = () => (
+  <div className="flex justify-center items-center lg:col-span-3">
+    <div className="mx-auto text-center space-y-5">
+      <img src="/images/empty-folder.svg" className="mx-auto" alt="empty-folder" />
+      <h2 className="font-medium text-xl text-[#000]">No Data</h2>
+    </div>
+  </div>
+);
+
+
--- components/index.js
+++ components/index.js
@@ -1,24 +1,37 @@
-
-
-import {CustomTableLoader} from "./common/CustomTableLoader";
-import {Footer} from "./common/Footer";
-import {Main} from "./common/Main";
-import {Header} from "./common/Header";
-import {Modal} from "./common/Modal";
-import {Sidebar} from "./common/Sidebar";
-import {Tab} from "./common/Tab";
-import  {
+import { CustomTableLoader } from "./common/CustomTableLoader";
+import { Footer } from "./common/Footer";
+import { EmptyRaiseExplore } from "./common/EmptyRaiseExplore";
+import { Main } from "./common/Main";
+import { Header } from "./common/Header";
+import { Modal } from "./common/Modal";
+import { Sidebar } from "./common/Sidebar";
+import { Tab } from "./common/Tab";
+import {
   Toaster,
   showToastErrorMessage,
   showToastInfoMessage,
   showToastMessage,
   showToastWarningMessage,
 } from "./common/Toaster";
-import {StudentActive} from "./teacherAndStudent/StudentActive";
-import {TeacherActive} from "./teacherAndStudent/TeacherActive";
-import {TeacherAndStudent} from "./teacherAndStudent/TeacherAndStudent";
+import { StudentActive } from "./teacherAndStudent/StudentActive";
+import { TeacherActive } from "./teacherAndStudent/TeacherActive";
+import { TeacherAndStudent } from "./teacherAndStudent/TeacherAndStudent";
 
 export {
-   CustomTableLoader, Footer, Header, Modal, Sidebar, StudentActive, Tab, TeacherActive, TeacherAndStudent, Toaster, showToastErrorMessage, showToastInfoMessage, showToastMessage, showToastWarningMessage, Main
+  CustomTableLoader,
+  Footer,
+  Header,
+  Modal,
+  Sidebar,
+  StudentActive,
+  Tab,
+  TeacherActive,
+  TeacherAndStudent,
+  Toaster,
+  showToastErrorMessage,
+  showToastInfoMessage,
+  showToastMessage,
+  showToastWarningMessage,
+  Main,
+  EmptyRaiseExplore,
 };
-
